868380d698c946ee6c14f7a3dad40f24d530e5d5
kelianf <kelianferhat@gmail.com>
2020-04-02 14:41:53 +0900

    Honed BackTest

--- app.py
+++ app.py
@@ -86,18 +86,20 @@ app.layout = html.Div([
             html.Div( className='row', style = {'display' : 'inline-flex', 'position': 'relative'},
                 children=[
                         html.Div([dcc.Input(id="CommoWeight1", value = 1, type='number', style={'width':'25%', 'position': 'absolute', 'bottom': '0','margin-left': '0px'})]),
-                        html.Div([ html.Label(["Pick a Commo:", dcc.Dropdown(id="CommoPick1", options=[{'label':x, 'value':x} for x in Liste], value="BO",style={'width': '100%'}  )])], style={'margin-left': '70px'}),
+                        html.Div([ html.Label(["Pick a Commo:", dcc.Dropdown(id="CommoPick1", options=[{'label':x, 'value':x} for x in Liste], value="BO",style={'width': '100%'}  )])], style={'margin-left': '75px'}),
                         html.Div([ html.Label(["Pick a Maturity:", dcc.Dropdown(id="MaturityPick1", options=[{'label':str(x), 'value':str(x)} for x in range(1,5)], value="1",style={'width': '100%'} )])], style={'margin-left': '5px'} ),
             ]),
             
             html.Div( className='row', style = {'display' : 'inline-flex', 'position': 'relative','margin-left': '100px'},
                 children=[
                         html.Div([dcc.Input(id="CommoWeight2", value = 0, type='number', style={'width':'25%', 'position': 'absolute', 'bottom': '0','margin-left': '0px'})]),
-                        html.Div([ html.Label(["Pick a Commo:", dcc.Dropdown(id="CommoPick2", options=[{'label':x, 'value':x} for x in Liste], value="BO",style={'width': '100%'}  )])], style={'margin-left': '70px'}),
+                        html.Div([ html.Label(["Pick a Commo:", dcc.Dropdown(id="CommoPick2", options=[{'label':x, 'value':x} for x in Liste], value="BO",style={'width': '100%'}  )])], style={'margin-left': '75px'}),
                         html.Div([ html.Label(["Pick a Maturity:", dcc.Dropdown(id="MaturityPick2", options=[{'label':str(x), 'value':str(x)} for x in range(1,5)], value="1",style={'width': '100%'} )])], style={'margin-left': '5px'} ),
             ]),
 
             dcc.Graph(id="ReturnsGraph"),
+            
+            dcc.Graph(id="ACF"),
             
 
             
@@ -122,20 +124,44 @@ def update_graph(Weight1, Commo1, Maturity1, Weight2, Commo2, Maturity2):
     if Weight2 != 0: Title = Title + Commo2
     if len(Title) > 2: Title = Title[:2] +" & " + Title[2:]
     df = Weight1 * Ret[Commo1+Maturity1] + Weight2 * Ret[Commo2+Maturity2]
-    
+    df = df+1
+    df.iloc[0] = 100   
+  
     return {
         'data': [    dict(x= pd.to_datetime(df.index, format="%Y%m%d") , 
-                          y= [x for x in (df.cumsum())], 
-                          name= 'Cumulated Returns'),
-                     dict(x= pd.to_datetime(df.index, format="%Y%m%d") , 
-                          y= [x for x in (df)], 
-                          name= 'Returns ')
+                          y= [x for x in df.cumprod()], 
+                          name= 'Cumulated Returns')
                  ],
-                          
         'layout': dict(title='Returns of ' + Title)
-        
         }
-    
+
+@app.callback(
+    Output("ACF", "figure"),
+    [Input("CommoWeight1", "value"), Input("CommoPick1", "value"), Input("MaturityPick1", "value"), Input("CommoWeight2", "value"), Input("CommoPick2", "value"), Input("MaturityPick2", "value")])
+def update_graph2(Weight1, Commo1, Maturity1, Weight2, Commo2, Maturity2):
+    Title = ""
+    if Weight1 is None:
+        Weight1 = 0
+    if Weight2 is None:
+        Weight2 = 0
+    if Weight1 != 0: Title = Title + Commo1
+    if Weight2 != 0: Title = Title + Commo2
+    if len(Title) > 2: Title = Title[:2] +" & " + Title[2:]
+    df = Weight1 * Ret[Commo1+Maturity1] + Weight2 * Ret[Commo2+Maturity2]
+    ACFListe = []
+    for x in range(60):
+        ACFListe.append( pd.concat((df, df.shift(x)), axis=1).dropna().corr().iloc[0,1] )
+    return {
+        'data': [    dict(x= [x for x in range(len(ACFListe))] , 
+                          y= [x for x in ACFListe], 
+                          type= 'bar',
+                          
+                          name= 'Cumulated Returns')
+                 ],
+        'layout': dict(title='AutoCorrelation Function',
+                       xaxis={'tickmode':'linear'},)
+        }
+
  
 @app.callback(
     [Output('Correltable', 'data'), Output('Correltable', 'columns')],
